31c36f5efad4c755c03d194be91131434d756a7f
elibrowne <eli.browne.23@bishops.com>
2021-09-15 12:28:03 -0700

    Finished ch. 9 - adaptive thresholding

---
+++ adaptive_thresholding.py
@@ -0,0 +1,23 @@
+import numpy as np
+import argparse
+import cv2 
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", required = True, help = "Path to the image")
+args = vars(ap.parse_args())
+
+image = cv2.imread(args["image"])
+image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+blurred = cv2.GaussianBlur(image, (5, 5), 0)
+cv2.imshow("Blurred Grayscale Image", blurred)
+cv2.waitKey(0)
+
+threshold = cv2.adaptiveThreshold(blurred, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY_INV, 11, 4)
+cv2.imshow("Mean Threshold", threshold)
+cv2.waitKey(0)
+
+threshold = cv2.adaptiveThreshold(blurred, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 15, 3)
+cv2.imshow("Gaussian Threshold", threshold)
+cv2.waitKey(0)
+
+# Picking between Gaussian and mean typically requires experimentation to figure out which one works better.
