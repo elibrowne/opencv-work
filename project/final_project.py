4cf8e24f48426d0ef6ac7f6ed26da5c480b52cd1
elibrowne <eli.browne.23@bishops.com>
2021-09-20 11:13:28 -0700

    Added RGB histogram, second image for testing

--- project/final_project.py
+++ project/final_project.py
@@ -1,3 +1,4 @@
+from matplotlib import pyplot
 import numpy as np
 import argparse
 import cv2
@@ -16,4 +17,22 @@ args = vars(ap.parse_args())
 # Opening processes - save and display the original image
 image = cv2.imread(args["image"])
 cv2.imshow("Original Image", image)
+cv2.waitKey(0) 
+
+# Creating a histogram for RGB colors - making the histogram
+channels = cv2.split(image) # splits into B G and R channels
+colors = ("b", "g", "r")
+pyplot.figure() # make a figure to add to
+pyplot.title("Color Prominence in Selected Image") 
+pyplot.xlabel("Bins")
+pyplot.ylabel("Num. Pixels")
+
+# Loop over the channels and respective colors to make histogram with three lines
+for (channel, color) in zip(channels, colors):
+	# For each color, make a new histogram with said color and add it to the plot
+	histogram = cv2.calcHist([channel], [0], None, [256], [0, 256]) # just like grayscale b/c only one color is shown
+	pyplot.plot(histogram, color = color) # set the color of the histogram to match the color it reflects
+	pyplot.xlim([0, 256]) # set the bounds as RGB
+
+pyplot.show() # show the combined RGB histogram
 cv2.waitKey(0) 
