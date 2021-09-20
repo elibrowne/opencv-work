2d07a3f24189dd191426a9632220d220951c5a38
elibrowne <eli.browne.23@bishops.com>
2021-09-20 11:05:43 -0700

    Added first image and basic starting code

--- project/final_project.py
+++ project/final_project.py
@@ -1,3 +1,19 @@
 import numpy as np
 import argparse
 import cv2
+
+##  Plan  ##
+# Open image (3)
+# Manipulate colors and display histogram (6, 7)
+# Draw a random amount of small shapes on the image (5)
+# Detect them and count them (and fill them in? inpainting) (9, 10, 11)
+
+# Opening processes - create ArgumentParser and parse arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", required = True, help = "Path to the image")
+args = vars(ap.parse_args())
+
+# Opening processes - save and display the original image
+image = cv2.imread(args["image"])
+cv2.imshow("Original Image", image)
+cv2.waitKey(0) 
